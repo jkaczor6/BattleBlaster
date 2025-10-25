a44f281f737979a2cd467de20338fdebf62d01c9
Jakub Kaczor <j.kaczor06@gmail.com>
2025-10-25 22:30:25 +0200

    Setting owner of projectile

--- Source/BattleBlaster/BasePawn.cpp
+++ Source/BattleBlaster/BasePawn.cpp
@@ -37,5 +37,10 @@ void ABasePawn::Fire()
 	FVector SpawnLocation = ProjectileSpawnPoint->GetComponentLocation();
 	FRotator SpawnRotation = ProjectileSpawnPoint->GetComponentRotation();
 
-	GetWorld()->SpawnActor<AProjectile>(ProjectileClass, SpawnLocation, SpawnRotation);
+	AProjectile* Projectile = GetWorld()->SpawnActor<AProjectile>(ProjectileClass, SpawnLocation, SpawnRotation);
+
+	if (Projectile)
+	{
+		Projectile->SetOwner(this);
+	}
 }
