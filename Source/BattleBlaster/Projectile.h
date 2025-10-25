4b9fb9ad8231e69e3e34048ac0b783777baa133b
Jakub Kaczor <j.kaczor06@gmail.com>
2025-10-25 22:56:06 +0200

    Created Health Component

--- Source/BattleBlaster/Projectile.h
+++ Source/BattleBlaster/Projectile.h
@@ -30,6 +30,9 @@ public:
 	UPROPERTY(VisibleAnywhere)
 	UProjectileMovementComponent* ProjectileMovemenetComponent;
 
+	UPROPERTY(EditAnywhere)
+	float Damage = 20.0f;
+
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 };
