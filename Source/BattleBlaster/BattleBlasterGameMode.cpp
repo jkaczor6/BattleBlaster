f90b2613fb2d97fa4b39afb4217d4188eb361cca
Jakub Kaczor <j.kaczor06@gmail.com>
2025-10-26 20:37:26 +0100

    Adding Victory/Defeat message in console temporary

--- Source/BattleBlaster/BattleBlasterGameMode.cpp
+++ Source/BattleBlaster/BattleBlasterGameMode.cpp
@@ -41,9 +41,13 @@ void ABattleBlasterGameMode::BeginPlay()
 
 void ABattleBlasterGameMode::ActorDied(AActor* DeadActor)
 {
+	bool IsGameOver = false;
+	bool IsVictory = false;
+
 	if (DeadActor == Tank)
 	{
 		Tank->HandleDestruction();
+		IsGameOver = true;
 	}
 	else
 	{
@@ -54,8 +58,16 @@ void ABattleBlasterGameMode::ActorDied(AActor* DeadActor)
 			TowerCount--;
 			if (TowerCount == 0)
 			{
-				UE_LOG(LogTemp, Display, TEXT("All Tower died, Victory!"));
+				IsGameOver = true;
+				IsVictory = true;
 			}
 		}
+	}
+
+	if (IsGameOver)
+	{
+		FString GameOverString = IsVictory ? "Victory!" : "Defeat!";
+
+		UE_LOG(LogTemp, Display, TEXT("Game over: %s"), *GameOverString);
 	}
 }
